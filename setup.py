f57a2ba31c5db4cea5bc39f666b689beb8dd0b01
Nimisha Nandakumar <nimishanandakumar777@gmail.com>
2024-06-06 23:33:36 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ="GenerativeAI projects",
+    version ="0.0.0.0",
+    author = "Nimisha Nandakumar",
+    author_email= "nimishanandakumar777@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
